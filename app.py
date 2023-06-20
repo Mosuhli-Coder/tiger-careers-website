d2c1558d6f05ec924d583b05fd03654b66538107
Mosuhli-Coder <70834953+Mosuhli-Coder@users.noreply.github.com>
2023-06-20 11:14:11 +0000

    Added the flask project

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_wolrd():
-    return "<p>Hello World!</p>"
+  return "<p>Hello Tiger!</p>"
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
